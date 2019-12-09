4cb04792ee146d36d51496f43c2f2a7a2f5f27ae
panqiang <1126525692@qq.com>
2019-12-09 20:39:17 +0800

    appbar

--- lib/CoinsRoute.dart
+++ lib/CoinsRoute.dart
@@ -11,12 +11,49 @@ class _CoinsRouteState extends State<CoinsRoute> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: Row(
-            children: <Widget>[
-              Text("我的房屋币")
-            ],
-          )
-        ),
+            constraints:
+                BoxConstraints(minWidth: double.infinity, minHeight: 40),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF00AAFF), Color(0xFF0085FF)],
+              ),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Positioned(
+                  left: 0,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
+                    child: Image(
+                      image: AssetImage("images/ic_back.webp"),
+                      height: 18,
+                      width: 18,
+                    ),
+                  ),
+                ),
+                Text(
+                  "我的房屋币",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 19,
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "规则",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
